17fb7e759b36dbb3e1c69b207ffdb48af410fecf
Vivian Crosby <design@vivianmaxine.com>
2018-10-06 15:45:25 -0500

    Utilizied keyword parameters in .py file and corresponding template

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def add_movie():
     # TODO:
     # Create a template called add-confirmation.html inside your /templates directory
     # Use that template to render the confirmation message instead of this temporary message below
-    return render_template('add-confirmation.html')
+    return render_template('add-confirmation.html', new_movie=new_movie)
 
 # TODO:
 # Modify the edit.html file to display the watchlist in an unordered list with bullets in front of each movie.
